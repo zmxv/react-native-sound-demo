9f25cdcd289f662594d0403c460e0e9ddfddb563
Ben Clayton <ben@calvium.com>
2017-02-08 14:27:17 +0000

    update AndroidManifest.xml / Activity / Application to newer RN format

---
+++ android/app/src/main/java/com/rnsounddemo/MainApplication.java
@@ -0,0 +1,34 @@
+package com.rnsounddemo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.zmxv.RNSound.RNSoundPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.asList(
+                    new MainReactPackage(),
+                    new RNSoundPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+}
