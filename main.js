9670e3c9d17935e772963b0d93d6e0858e8f920e
Ben Clayton <ben@calvium.com>
2017-02-14 16:49:34 +0000

    Add iOS example of using 'require' to play an audio file.

--- main.js
+++ main.js
@@ -4,36 +4,72 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  Platform,
 } from 'react-native';
 const Sound = require('react-native-sound');
+
+const Button = ({title, onPress}) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.button}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const Header = ({children}) => (<Text style={styles.header}>{children}</Text>);
+
+const Feature = ({title, onPress, description, buttonLabel = "PLAY"}) => (
+  <View style={styles.feature}>
+    <Header>{title}</Header>
+    <Button title={buttonLabel} onPress={onPress}/>
+  </View>);
+
+const requireAudio = require('./advertising.mp3');
 
 class MainView extends Component {
 
   constructor(props) {
     super(props);
 
-    Sound.setCategory('Ambient', true); // mixWithOthers
+    Sound.setCategory('Ambient', true); // true = mixWithOthers
+
+    this.playSoundBundle = () => {
+      const s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
+        if (e) {
+          console.log('error', e);
+        } else {
+          s.setSpeed(1);
+          console.log('duration', s.getDuration());
+          s.play(() => s.release()); // Release when it's done so we're not using up resources
+        }
+      });
+    };
+
+    this.playSoundFromRequire = () => {
+      const s = new Sound(requireAudio, (e) => {
+        if (e) {
+          console.log('error', e);
+          return;
+        }
+
+        s.play(() => s.release());
+      });
+    };
+
+
+
+  }
+
+  renderiOSOnlyFeatures() {
+    return [
+      <Feature key="require" title="Audio via 'require' statement" onPress={this.playSoundFromRequire}/>,
+    ]
   }
 
   render() {
     return <View style={styles.container}>
-      <TouchableOpacity onPress={this.playSound}>
-        <Text style={styles.button}>play</Text>
-      </TouchableOpacity>
-    </View>;
-  }
-
-  playSound() {
-    const s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
-      if (e) {
-        console.log('error', e);
-      } else {
-        s.setSpeed(1);
-        console.log('duration', s.getDuration());
-        s.play();
-      }
-    });
+      <Feature title="Main bundle audio" onPress={this.playSoundBundle}/>
+      { Platform.OS === 'ios' ? this.renderiOSOnlyFeatures() : null }
+    </View>
   }
 }
 
@@ -49,6 +85,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'silver',
     padding: 5,
   },
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+  },
+  feature: {
+    padding: 20,
+    alignSelf: 'stretch',
+  }
 });
 
 export default MainView;
