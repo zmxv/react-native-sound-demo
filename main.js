9d55b9e3df0f08fd92598bb6963048b2e7e1ae7e
Ben Clayton <ben@calvium.com>
2017-05-12 20:56:31 +0100

    WIP adding play sound from require() and from network

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ import {
   TouchableOpacity,
   View,
   Platform,
+  Alert,
+  ScrollView,
+  InteractionManager,
 } from 'react-native';
+
 const Sound = require('react-native-sound');
 
 const Button = ({title, onPress}) => (
@@ -20,10 +24,42 @@ const Header = ({children}) => (<Text style={styles.header}>{children}</Text>);
 const Feature = ({title, onPress, description, buttonLabel = "PLAY"}) => (
   <View style={styles.feature}>
     <Header>{title}</Header>
-    <Button title={buttonLabel} onPress={onPress}/>
+    <Button title={buttonLabel} onPress={() => InteractionManager.runAfterInteractions(onPress)}/>
   </View>);
 
 const requireAudio = require('./advertising.mp3');
+
+const remoteAudio = `https://languagezenstorage.blob.core.windows.net/media0/xgcUXjHhP8.mp3`;
+const noop = () => {
+};
+
+/**
+ * @param uri - of sound to play
+ * @param basePath - e.g. Sound.MAIN_BUNDLE or '' for URLs
+ * @param onReady - callback to run when sound is ready to go, and before we call play. You're passed the react-native-sound instance. Useful for setting up loops etc
+ * @returns {Promise} that resolves when the sound has been played, or rejects with an error
+ */
+function playAudio(uri, basePath = '', onReady = noop) {
+  return new Promise((res, rej) => {
+    const s = new Sound(uri, basePath, (e) => {
+      if (e) {
+        handleError(e);
+        rej(e);
+        return;
+      }
+      onReady(s);
+      s.play(() => {
+        res();
+        s.release();
+      }); // Release when it's done so we're not using up resources
+    })
+  });
+}
+
+function handleError(e) {
+  Alert.alert(`Error: ${e.message} code: ${e.code}`);
+  console.log('error', e);
+}
 
 class MainView extends Component {
 
@@ -32,30 +68,26 @@ class MainView extends Component {
 
     Sound.setCategory('Ambient', true); // true = mixWithOthers
 
+
     this.playSoundBundle = () => {
-      const s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
-        if (e) {
-          console.log('error', e);
-        } else {
-          s.setSpeed(1);
-          console.log('duration', s.getDuration());
-          s.play(() => s.release()); // Release when it's done so we're not using up resources
-        }
-      });
+      playAudio('advertising.mp3', Sound.MAIN_BUNDLE)
+        .catch(e => console.log(e))
+    };
+
+    this.playSoundBundleNotFound = () => {
+      playAudio('this_file_does_not_exist.mp3', Sound.MAIN_BUNDLE)
+        .catch(e => console.log(e))
     };
 
     this.playSoundLooped = () => {
       if (this.state.loopingSound) {
         return;
       }
-      const s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
-        if (e) {
-          console.log('error', e);
-        }
+      playAudio('advertising.mp3', Sound.MAIN_BUNDLE, (s) => {
         s.setNumberOfLoops(-1);
-        s.play();
-      });
-      this.setState({loopingSound: s});
+        this.setState({loopingSound: s});
+      })
+        .catch(e => console.log(e));
     };
 
     this.stopSoundLooped = () => {
@@ -70,36 +102,42 @@ class MainView extends Component {
     };
 
     this.playSoundFromRequire = () => {
-      const s = new Sound(requireAudio, (e) => {
-        if (e) {
-          console.log('error', e);
-          return;
-        }
+      playAudio(requireAudio)
+        .catch(e => console.log(e))
+    };
 
-        s.play(() => s.release());
-      });
+    this.playSoundUrl = () => {
+      if (this.state.preparing) {
+        return;
+      }
+      this.setState({preparing: true});
+      playAudio(remoteAudio, '', s => this.setState({preparing: false}))
+        .catch(e => console.log(e))
     };
 
     this.state = {
       loopingSound: undefined,
+      preparing: false,
     };
   }
 
-  renderiOSOnlyFeatures() {
-    return [
-      <Feature key="require" title="Audio via 'require' statement" onPress={this.playSoundFromRequire}/>,
-    ]
-  }
 
   render() {
-    return <View style={styles.container}>
-      <Feature title="Main bundle audio" onPress={this.playSoundBundle}/>
-      {this.state.loopingSound
-        ? <Feature title="Main bundle audio (looped)" buttonLabel={'STOP'} onPress={this.stopSoundLooped}/>
-        : <Feature title="Main bundle audio (looped)" buttonLabel={'PLAY'} onPress={this.playSoundLooped}/>
-      }
-      { Platform.OS === 'ios' ? this.renderiOSOnlyFeatures() : null }
-    </View>
+    return <ScrollView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Feature title="Main bundle audio" onPress={this.playSoundBundle}/>
+        {this.state.preparing
+          ? <Feature title="Audio at remote URL" buttonLabel="Preparing..."/>
+          : <Feature title="Audio at remote URL" onPress={this.playSoundUrl}/>
+        }
+        <Feature title="Non-existent bundle audio" onPress={this.playSoundBundleNotFound}/>
+        <Feature key="require" title="Audio via 'require' statement" onPress={this.playSoundFromRequire}/>
+        {this.state.loopingSound
+          ? <Feature title="Main bundle audio (looped)" buttonLabel="STOP" onPress={this.stopSoundLooped}/>
+          : <Feature title="Main bundle audio (looped)" buttonLabel="PLAY" onPress={this.playSoundLooped}/>
+        }
+      </View>
+    </ScrollView>
   }
 }
 
